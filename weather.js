1a13b03f53135573952a0c5e07feb5b0435210cc
madiinemm <uraimovamadina179@gmail.com>
2024-01-12 22:45:57 +0500

    weather application completed

---
+++ weather.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container');
+const button = document.createElement('button');
+const weatherBox = document.getElementById('weather-box');
+const weatherDetails = document.getElementById('weather-details');
+
+button && button.addEventListener('click', (e) => {
+    const APIKey = ''
+})
